d3f861f04bba289051c0714acac36148dde5a80b
AviShi1 <avishilon26@gmail.com>
2022-03-13 09:43:36 -0700

    3rd commit

--- avishilon1.java
+++ avishilon1.java
@@ -1,7 +1,7 @@
 public class avishilon1{
 	public static void main(String[] args) {
 		for(int i=1;i<=10;i++){
-			System.out.println("avi shilon1...."+i);
+			System.out.println("avi shilon2221...."+i);
 		}
 	}
 }
