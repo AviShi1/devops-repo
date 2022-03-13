d94bbd607b726d4356485964f51b7eb4dcdf62e5
AviShi1 <avishilon26@gmail.com>
2022-03-13 10:02:41 -0700

    1st

--- avishilon1.java
+++ avishilon1.java
@@ -1,7 +1,7 @@
 public class avishilon1{
 	public static void main(String[] args) {
 		for(int i=1;i<=10;i++){
-			System.out.println("avi-shilon254152653151...."+i);
+			System.out.println("avi-shilon25411152653151...."+i);
 		}
 	}
 }
